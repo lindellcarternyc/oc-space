c845e2b724b566f3594a95081e1fbf8ac893775b
Lindell Carter <lindellcarternyc@gmail.com>
2018-02-03 21:48:24 -0500

    split sign-in component

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 
-import { Header, Form, Input, Message } from 'semantic-ui-react'
-
+import { Header } from 'semantic-ui-react'
 import Page from '../../components/page'
+import SignInForm from './sign-in-form'
 
-import { isValidEmail, isValidPassword } from '../../utils'
-import { getUserForEmail } from '../../data/user-data'
 import { User } from '../../types/user-types'
 
 import { connect } from 'react-redux'
@@ -16,193 +14,24 @@ import { signIn } from '../../actions/auth-actions'
 import { navigateToPage } from '../../actions/navigation-actions'
 import * as NavTypes from '../../types/navigation-types'
 
-interface SignInState {
-  email: {
-    value: string
-    error?: string | undefined
-  }
-  password: {
-    value: string
-    error?: string | undefined
-  }
-  error?: {
-    title: string
-    message: string
-  } | undefined
-}
 interface SignInProps {
   signIn: (user: User) => void
   goToHome: () => void
 }
-class SignIn extends React.Component<SignInProps, SignInState> {
+class SignIn extends React.Component<SignInProps> {
   constructor(props: SignInProps) {
-    super(props)
-
-    this.state = {
-      email: {
-        value: 'lindellcarternyc@gmail.com',
-        error: undefined
-      },
-      password: {
-        value: 'password1',
-        error: undefined
-      },
-      error: undefined
-    }
+    super(props) 
   }
 
-  handleChange = (evt: React.SyntheticEvent<HTMLInputElement>, data: {name: string, value: string}) => {
-    const { name, value } = data
-    
-    switch (name) {
-      case 'email':
-        const email = {...this.state.email, value}
-        this.setState({email}, () => {
-          this.validateEmail()
-        })
-        break
-
-      case 'password':
-        const password = {...this.state.password, value}
-        this.setState({password}, () => {
-          this.validatePassword()
-        })
-        break
-
-      default: return
-    }
+  onSubmit = (user: User) => {
+    this.props.signIn(user)
+    this.props.goToHome()
   }
-
-  onBlur = (evt: React.SyntheticEvent<HTMLInputElement>) => {
-    switch (evt.currentTarget.name) {
-      case 'email':
-        this.validateEmail()
-        break
-      
-      case 'password':
-        this.validatePassword()
-        break
-      default: return
-    }
-  }
-
-  validateEmail() {
-    let { email } = this.state
-
-    const validEmail = isValidEmail(email.value)
-    if (validEmail) {
-      email = {...email, error: undefined}
-    } else {
-      email = {...email, error: 'Invalid email'}
-    }
-
-    this.setState({email}, () => {
-      this.handleErrors()
-    })
-  }
-
-  validatePassword() {
-    let { password } = this.state
-
-    if (isValidPassword(password.value)) {
-      password = {...password, error: undefined}
-    } else {
-      password = {...password, error: 'Invalid password'}
-    }
-
-    this.setState({password}, () => {
-      this.handleErrors()
-    })
-  }
-
-  isValid() {
-    const { email, password, error } = this.state
-
-    return isValidEmail(email.value) && isValidPassword(password.value) && error === undefined
-  }
-
-  handleErrors() {
-    const { email, password } = this.state
-    const emailError = email.error
-    const passwordError = password.error
-
-    let error: {title: string, message: string} | undefined
-    
-    if (emailError) {
-      if (emailError === 'Invalid email') {
-        error = {
-          title: emailError,
-          message: 'Please enter a valid email'
-        }
-        this.setState({error})
-      }
-    } else if (passwordError) {
-      if (passwordError === 'Invalid password') {
-        error = {
-          title: passwordError,
-          message: 'Please enter a valid password'
-        }
-        this.setState({error})
-      }
-    } else {
-      this.setState({error: undefined})
-    }
-  }
-
-  onSubmit = (evt: React.SyntheticEvent<HTMLFormElement | HTMLButtonElement>) => {
-    evt.stopPropagation()
-    evt.preventDefault()
-
-    if (this.isValid()) {
-      const { email, password } = this.state
-      const user = getUserForEmail(email.value)
-      if (user !== null) {
-        if (user.password && user.password === password.value) {
-          this.props.signIn(user)
-          this.props.goToHome()
-        }
-      }
-    } else {
-      return
-    }
-  }
-
   render() {
-    const { error } = this.state
     return (
       <Page>
         <Header as='h2' content='Sign In' />
-        <Form warning={error !== undefined} onSubmit={this.onSubmit}>
-          <Form.Field 
-            label='Email' 
-            control={Input}
-            name='email'
-            onChange={this.handleChange}
-            onBlur={this.onBlur}
-          />
-          <Form.Field 
-            label='Password' 
-            control={Input} 
-            type='password'
-            name='password'
-            onChange={this.handleChange}
-            onBlur={this.onBlur}
-          />
-          <Message 
-            warning
-            header={error ? error.title : null}
-            content={error ? error.message : null}
-          />
-          <Form.Group>
-            <Form.Button 
-              content='Sign In' 
-              color='blue' 
-              disabled={!this.isValid()}
-              onClick={this.onSubmit}
-            />
-            <Form.Button content='Cancel' color='black'/>
-          </Form.Group>
-        </Form>
+        <SignInForm onSubmit={this.onSubmit}/>
       </Page>
     )
   }
