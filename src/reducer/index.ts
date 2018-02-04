01b7da8f9cd9df61b2e306c7837e4a70a665e91b
Lindell Carter <lindellcarternyc@gmail.com>
2018-02-03 19:08:45 -0500

    add navigation to store

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -3,10 +3,12 @@ import StoreState from '../store/state'
 
 import navigationReducer from './navigation-reducer'
 import performancesReducer from './performances-reducer'
+import authReducer from './auth-reducer'
 
 const rootReducer = combineReducers<StoreState>({
   navigationState: navigationReducer,
-  performancesState: performancesReducer
+  performancesState: performancesReducer,
+  authState: authReducer
 })
 
 export default rootReducer
--- src/store/state.ts
+++ src/store/state.ts
@@ -1,9 +1,11 @@
 import NavigationState from './navigation-state'
 import PerformancesState from './performances-state'
+import AuthState from './auth-state'
 
 interface StoreState {
   navigationState: NavigationState,
-  performancesState: PerformancesState
+  performancesState: PerformancesState,
+  authState: AuthState
 }
 
 export default StoreState
