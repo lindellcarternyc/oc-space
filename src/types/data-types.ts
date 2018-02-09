20ea9ebb0dd48ae9a6bdaf6388360080fbf199a1
Lindell Carter <lindellcarternyc@gmail.com>
2018-02-08 23:46:34 -0500

    added singer type and no location

--- src/types/data-types.ts
+++ src/types/data-types.ts
@@ -2,11 +2,13 @@ export enum Location {
   TimesSquare = 'Times Square',
   PortAuthority = 'Port Authority',
   HeraldSquare = 'Herald Square',
-  UnionSquare = 'Union Square'
+  UnionSquare = 'Union Square',
+  NONE = 'NONE'
 }
 
 export interface Performance {
   location: Location
   date: string
   time: string
+  singerIDs?: string[]
 }
